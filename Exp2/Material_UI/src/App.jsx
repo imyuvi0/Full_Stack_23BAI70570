1b59907e3f7dcc2d38c7e0e904f37eea780d04d9
imyuvi0 <jk0424366@gmail.com>
2026-01-14 12:35:20 +0530

    push

--- Exp2/Material_UI/src/App.jsx
+++ Exp2/Material_UI/src/App.jsx
@@ -1,13 +1,27 @@
-import { Button, TextField, Card, CardContent } from '@mui/material';
+import { Button, TextField, Card, CardContent, Box } from '@mui/material';
+
 function App() {
-return (
-<Card style={{ width: 300, margin: '20px auto' }}>
-<CardContent>
-<h3>Material UI Form</h3>
-<TextField label="Name" fullWidth margin="normal" />
-<Button variant="contained" fullWidth>Submit</Button>
-</CardContent>
-</Card>
-);
+  return (
+    <Box
+      sx={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Card sx={{ width: 300 }}>
+        <CardContent>
+          <h3>Material UI Form</h3>
+          <TextField label="Name" fullWidth margin="normal" />
+          <Button variant="contained" fullWidth>
+            Submit
+          </Button>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
+
 export default App;
